461c68b371fb07514a80e8605bf96be5a09258a4
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-08-22 10:59:43 -0400

    Can now hold dice when rerolling

--- src/App.js
+++ src/App.js
@@ -4,19 +4,43 @@ import Die from "./components/Die"
 export default function App(){
 
     function allNewDice(){
-        const randValues = []
+        const newDice = []
         for (var i=1; i<=10; i++){
-            randValues.push( Math.ceil(Math.random() * 6 ) ) //Random value from 1 to 6
+            newDice.push(
+                {
+                    id: i,
+                    value: Math.ceil(Math.random() * 6 ),  //Random value from 1 to 6
+                    isHeld: false,
+                }
+            )
         }
-        return randValues
+        return newDice
+    }
+
+    function toggleHold(diceId){
+        return setDice(prevState => {
+            return prevState.map(die => die.id===diceId ? {...die, isHeld: !die.isHeld} : {...die})
+        })
     }
 
     const [dice, setDice] = React.useState(allNewDice())
-    const diceElements = dice.map(val => (<Die value={val}/>))
+    const diceElements = dice.map(die => (
+        <Die
+            key={die.id}
+            value={die.value}
+            isHeld={die.isHeld}
+            onClick={() => toggleHold(die.id)}
+        />
+    ))
 
     function reRoll(){
-        setDice(prevDice => prevDice = allNewDice())
+        setDice(prevDice => {
+            return prevDice.map(
+                die => die.isHeld ? {...die} : {...die, value: Math.ceil(Math.random() * 6 )}
+            )
+        })
     }
+
 
     return (
         <main className="m-5 p-5 rounded-3">
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,6 +1,6 @@
 export default function Die(props){
     return (
-        <div className="die shadow border bg-white text-center mx-1 my-4">
+        <div id={`die-${props.id}`} className={`die shadow border text-center mx-1 my-4 ${props.isHeld ? "bg-success text-white" : "bg-white text-black"}`} onClick={props.onClick}>
             <h1 className="die--value mt-2">{props.value}</h1>
         </div>
     )
