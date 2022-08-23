5074e4c73543ec3e4987a6e6f7df57af984f6cfe
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-08-23 09:41:33 -0400

    Cleaner assignment of classes

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,6 +1,9 @@
 export default function Die(props){
+    const classes = "die shadow border text-center mx-1 my-4 " 
+        + (props.isHeld ? "bg-success text-white" : "bg-white text-black")
+
     return (
-        <div id={`die-${props.id}`} className={`die shadow border text-center mx-1 my-4 ${props.isHeld ? "bg-success text-white" : "bg-white text-black"}`} onClick={props.onClick}>
+        <div id={`die-${props.id}`} className={classes} onClick={props.onClick}>
             <h1 className="die--value mt-2">{props.value}</h1>
         </div>
     )
